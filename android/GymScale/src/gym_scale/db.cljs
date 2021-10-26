1e7f5c939186a4595d1f3683f3b31e4545fdd37d
Juan Monetta <jpmonettas@gmail.com>
2021-10-26 08:37:09 -0300

    Add spec checking on re-frame db

--- android/GymScale/src/gym_scale/db.cljs
+++ android/GymScale/src/gym_scale/db.cljs
@@ -4,7 +4,7 @@
 (s/def :scale/last-weight number?)
 (s/def :scale/connected? boolean?)
 
-(s/def ::db (s/keys :req-un [:scale/last-weight
+(s/def ::db (s/keys :req [:scale/last-weight
                              :scale/connected?]))
 
 (def initial-db
--- android/GymScale/src/gym_scale/events.cljs
+++ android/GymScale/src/gym_scale/events.cljs
@@ -1,65 +1,75 @@
 (ns gym-scale.events
-  (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx dispatch]]
+  (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx dispatch] :as re-frame]
             [gym-scale.db :as db]
             [gym-scale.fxs.ble]
-            [gym-scale.fxs.sqlite]))
+            [gym-scale.fxs.sqlite]
+            [expound.alpha :as expound]
+            [clojure.spec.alpha :as s]))
 
-(reg-event-fx
- :app/init
- (fn [_ _]
-   {:db db/initial-db
-    :sqlite/open {:db-name "GymScale"
-                  :db-version "1.0"
-                  :db-display-name "SQLite gym scale database"
-                  :db-size 200000}}))
+(defn check-and-throw
+  "Throws an exception if `db` doesn't match the Spec `a-spec`."
+  [a-spec db]
+  (when-not (s/valid? a-spec db)
+    (throw (js/Error. (str "spec check failed: " (expound/expound-str a-spec db))))))
 
-(reg-event-fx
- :scale/open-connection
- (fn [cofxs _]
-   {:ble/start nil}))
+(def sc (re-frame/after (partial check-and-throw ::db/db)))
 
-(reg-event-fx
- :scale/close-connection
- (fn [cofxs _]
-   {:serial/close-connection nil}))
+(defn init [_ _]
+  {:db db/initial-db
+   :sqlite/open {:db-name "GymScale"
+                 :db-version "1.0"
+                 :db-display-name "SQLite gym scale database"
+                 :db-size 200000}})
 
-(reg-event-db
- :scale/on-weight-change
- (fn [db [_ w]]
-   (assoc db :scale/last-weight w)))
+(reg-event-fx :app/init [sc] init)
 
-(reg-event-db
- :scale/connected
- (fn [db _]
-   (assoc db :scale/connected? true)))
+(defn open-connection [cofxs _]
+  {:ble/start nil})
 
-(reg-event-db
- :scale/disconnected
- (fn [db _]
-   (assoc db :scale/connected? false)))
+(reg-event-fx :scale/open-connection [sc] open-connection)
 
-(reg-event-db
- :sqlite-db/opened
- (fn [db _]
+(defn close-connection [cofxs _]
+  {:serial/close-connection nil})
+
+(reg-event-fx :scale/close-connection [sc] close-connection)
+
+(defn on-weight-change [db [_ w]]
+  (assoc db :scale/last-weight w))
+
+(reg-event-db :scale/on-weight-change [] on-weight-change)
+
+(defn connected [db _]
+  (assoc db :scale/connected? true))
+
+(reg-event-db :scale/connected [sc] connected)
+
+(defn disconnected [db _]
+  (assoc db :scale/connected? false))
+
+(reg-event-db :scale/disconnected [sc] disconnected)
+
+(defn db-opened [db _]
    (js/console.log "DB opened")
-   db))
+  db)
 
-(reg-event-db
- :sqlite-db/error
- (fn [db [_ err]]
+(reg-event-db :sqlite-db/opened [sc] db-opened)
+
+(defn db-error [db [_ err]]
    (js/console.error err)
-   db))
+  db)
 
-(reg-event-db
- :sqlite-db/users-loaded
- (fn [db [_ all-users]]
+(reg-event-db :sqlite-db/error [sc] db-error)
+
+(defn users-loaded [db [_ all-users]]
    (js/console.log "All users" (clj->js all-users))
-   db))
+  db)
 
-(reg-event-fx
- :sqlite-db/load-all-users
- (fn [_ _]
-   {:sqlite/execute-sql {:query "SELECT * from users;"
-                         :params-vec []
-                         :succ-ev [:db/users-loaded]
-                         :err-ev  [:db/error]}}))
+(reg-event-db :sqlite-db/users-loaded [sc] users-loaded)
+
+(defn load-users [_ _]
+  {:sqlite/execute-sql {:query "SELECT * from users;"
+                        :params-vec []
+                        :succ-ev [:db/users-loaded]
+                        :err-ev  [:db/error]}})
+
+(reg-event-fx :sqlite-db/load-all-users [sc] load-users)
